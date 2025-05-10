dc9627dadc1a76abd34e50bb7aefa19f540a90bf
Nickkey <nick.poommipat@gmail.com>
2025-05-10 22:17:13 +0700

    first

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
+func main() {
+	app := fiber.New()
+
+	books = append(books, Book{ID: 1, Title: "Hello", Author: "Nick"})
+	books = append(books, Book{ID: 2, Title: "World", Author: "Nick"})
+
+	//routes
+	app.Get("/books", getBooks)
+	app.Get("/books/:id", getBook)
+	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
+	app.Delete("/books/:id",deleteBook)
+
+
+	app.Listen(":8080")
+}
+
